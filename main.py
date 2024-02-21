68fdcd423bc3d5ff1312633e313ce6626012fbe3
robin <prl90777@gmail.com>
2024-02-20 20:02:28 -0500

    adding model and eval, train, and preict function

--- main.py
+++ main.py
@@ -5,8 +5,19 @@ from data_processing.data_layer import DataParsing
 from data_processing.data_loader import PIIDataset, CustomCollateFn
 from utils.misc import flatten_out_list, build_vocab
 from sklearn.model_selection import train_test_split
-from torch.utils.data import DataLoader
 import torch
+from torch.utils.data import DataLoader
+from models.bilstm_crf import BiLSTM_CRF
+from process_loops.train_text import train, evaluate, predict
+
+
+def load_data(p):
+    df = (pd.read_json(p)
+          .head(100)
+          .reset_index()
+          )
+    return df
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
@@ -20,18 +31,18 @@ if __name__ == '__main__':
     test_size = 0.2
     random_state = 42
     batch_size = 16
+    train_path = 'data/train.json'
+    test_path = 'data/test.json'
+
+    df_train = load_data(train_path)
+    df_test = load_data(test_path)
 
     dp = DataParsing(chunk_size=chunk_size, overlap=overlap)
 
-    df = (pd.read_json('data/train.json')
-          .head(100)
-          .reset_index()
-          )
-
-    unique_tokens = flatten_out_list(df.tokens.to_list())
-    unique_labels = flatten_out_list(df.labels.to_list())
-
-    x_train, x_val = train_test_split(df, test_size=test_size, random_state=random_state)
+    x_train, x_val = train_test_split(df_train,
+                                      test_size=test_size,
+                                      random_state=random_state
+                                      )
 
     x_train = dp.fit_transform(x_train)
 
@@ -40,24 +51,24 @@ if __name__ == '__main__':
     vocab_tokens = build_vocab(x_train.tokens.to_list())
     labels_tokens = build_vocab(x_train.labels.to_list())
     pos_tokens = build_vocab(x_train.pos.to_list())
-
-    x_train_t = PIIDataset(x_train,
-                           word_to_idx=vocab_tokens,
-                           pos_to_idx=pos_tokens,
-                           label_to_idx=labels_tokens)
-
-    collate_fn = CustomCollateFn(chunk_size=chunk_size,
-                                 word_to_idx=vocab_tokens,
-                                 pos_to_idx=pos_tokens,
-                                 label_to_idx=labels_tokens)
-    loader = DataLoader(x_train_t,
-                        batch_size= batch_size,
-                        collate_fn=collate_fn)
-
-    for t,p,l in loader:
-        tokens_padded, pos_tags_padded, labels_padded = t.to(device), p.to(device), l.to(device)
-        print("Tokens Shape:", tokens_padded.shape)
-        print("POS Tags Shape:", pos_tags_padded.shape)
-        print("Labels Shape:", labels_padded.shape)
-
-    # pdb.set_trace()
+    #
+    # x_train_t = PIIDataset(x_train,
+    #                        word_to_idx=vocab_tokens,
+    #                        pos_to_idx=pos_tokens,
+    #                        label_to_idx=labels_tokens)
+    #
+    # collate_fn = CustomCollateFn(chunk_size=chunk_size,
+    #                              word_to_idx=vocab_tokens,
+    #                              pos_to_idx=pos_tokens,
+    #                              label_to_idx=labels_tokens)
+    # loader = DataLoader(x_train_t,
+    #                     batch_size= batch_size,
+    #                     collate_fn=collate_fn)
+    #
+    # for t,p,l in loader:
+    #     tokens_padded, pos_tags_padded, labels_padded = t.to(device), p.to(device), l.to(device)
+    #     print("Tokens Shape:", tokens_padded.shape)
+    #     print("POS Tags Shape:", pos_tags_padded.shape)
+    #     print("Labels Shape:", labels_padded.shape)
+    #
+    # # pdb.set_trace()
--- models/bilstm_crf.py
+++ models/bilstm_crf.py
@@ -1,29 +1,49 @@
 import torch
 import torch.nn as nn
-from torchcrf import CRF
+from TorchCRF import CRF
+
+import torch
+import torch.nn as nn
+from TorchCRF import CRF
 
 
 class BiLSTM_CRF(nn.Module):
-    def __init__(self, vocab_size, pos_vocab_size, embedding_dim, hidden_dim, num_tags):
+    def __init__(self, vocab_size, pos_vocab_size, embedding_dim, hidden_dim, num_tags, pad_token_index, num_layers=1,
+                 dropout_rate=.001):
         super(BiLSTM_CRF, self).__init__()
+        self.num_layers = num_layers
+        self.dropout_rate = dropout_rate
+        self.pad_token_index = pad_token_index  # Padding token index for mask calculation
+
         self.token_embedding = nn.Embedding(vocab_size, embedding_dim)
         self.pos_embedding = nn.Embedding(pos_vocab_size, embedding_dim)
-        self.bi_lstm = nn.LSTM(embedding_dim * 2, hidden_dim // 2,
-                               num_layers=1, bidirectional=True, batch_first=True)
+
+        self.bi_lstm = nn.LSTM(embedding_dim * 2,
+                               hidden_dim // 2,
+                               num_layers=self.num_layers,
+                               bidirectional=True,
+                               batch_first=True,
+                               dropout=self.dropout_rate if num_layers > 1 else 0)
+
         self.hidden2tag = nn.Linear(hidden_dim, num_tags)
-        self.crf = CRF(num_tags, batch_first=True)
+
+        self.crf = CRF(num_tags)
 
     def forward(self, tokens, pos_tags, labels=None):
+
         token_embeddings = self.token_embedding(tokens)
         pos_embeddings = self.pos_embedding(pos_tags)
-
         embeddings = torch.cat((token_embeddings, pos_embeddings), dim=-1)
+
         lstm_out, _ = self.bi_lstm(embeddings)
+
         emissions = self.hidden2tag(lstm_out)
 
+        mask = (tokens != self.pad_token_index)
+
         if labels is not None:
-            loss = -self.crf(emissions, labels)
-            return loss
+
+            return -self.crf(emissions, labels, mask=mask)
         else:
-            predictions = self.crf.decode(emissions)
-            return predictions
+
+            return self.crf.decode(emissions)
